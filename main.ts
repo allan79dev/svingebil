5a00b193dd9ecff8adea136e496e1b5eaad0ec80
allan79dev <82371470+allan79dev@users.noreply.github.com>
2022-08-09 19:52:02 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P1.run(-50)
+    basic.pause(100)
+    servos.P1.stop()
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P1.run(50)
+    basic.pause(100)
+    servos.P1.stop()
+})
 basic.forever(function () {
-	
+    if (input.rotation(Rotation.Pitch) < -30) {
+        servos.P2.run(100)
+    } else if (input.rotation(Rotation.Pitch) > 40) {
+        servos.P2.run(-50)
+    } else {
+        servos.P2.stop()
+    }
 })
